30d2dd5aefee98d4222ffa74504a125bd81fa81b
barış bar <barisbar79@gmail.com>
2022-12-16 12:29:09 +0300

    20221216-1229

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import conn from "./services/mongoose.service.js";
-
 import routeManager from "./routes/route.manager.js"
 
 dotenv.config();
 
-conn();
+
 
 const app = express();
 const port = process.env.PORT || 3000;
-
+conn();
 //ejs template engine
 app.set('view engine', 'ejs');
 
@@ -20,8 +19,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
 
-app.get("/", (req, res) => { res.render('index') });
+
 /*
+app.get("/", (req, res) => { res.render('index') });
 app.get("/about", (req, res) => { res.render('about') });
 app.get("/work", (req, res) => { res.render('work') });
 app.get("/contact", (req, res) => { res.render('contact') });
